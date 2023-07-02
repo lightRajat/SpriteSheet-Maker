80da756f5d9aae4d21a9a6f8e1ea764813bb26bc
Rajat <light.rajat+gh@gmail.com>
2023-07-02 23:01:30 +0530

    Added GUI starter boilerplate

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-from PIL import Image
-from os import listdir
+from tkinter import Tk
 
-hor = True
-folder = "sprites"
-sprites = listdir(folder)
-outFolder = "./.."
-outName = "result"
+window = Tk()
+window.title("SpriteSheet Maker")
 
-testImg = Image.open("{}/{}".format(folder, sprites[0]))
-imgWidth = testImg.width
-imgHeight = testImg.height
-imgFormat = testImg.format
-del testImg
+window.mainloop()
 
-img = Image.new('RGBA', (imgWidth * len(sprites)**hor, imgHeight * len(sprites)**int(not(hor)) ))
-co = 0
-for i in sprites:
-    sprite = Image.open("{}/{}".format(folder, i))
-    img.paste(sprite, (co * hor, co * int(not(hor))))
-    co += imgWidth
-img.save("{}/{}.{}".format(outFolder, outName, imgFormat.lower()))
+##from PIL import Image
+##from os import listdir
+##
+##hor = True
+##folder = "sprites"
+##sprites = listdir(folder)
+##outFolder = "./.."
+##outName = "result"
+##
+##testImg = Image.open("{}/{}".format(folder, sprites[0]))
+##imgWidth = testImg.width
+##imgHeight = testImg.height
+##imgFormat = testImg.format
+##del testImg
+##
+##img = Image.new('RGBA', (imgWidth * len(sprites)**hor, imgHeight * len(sprites)**int(not(hor)) ))
+##co = 0
+##for i in sprites:
+##    sprite = Image.open("{}/{}".format(folder, i))
+##    img.paste(sprite, (co * hor, co * int(not(hor))))
+##    co += imgWidth
+##img.save("{}/{}.{}".format(outFolder, outName, imgFormat.lower()))
