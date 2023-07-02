78aeea5cf822de690a358156227fdd5c8defd082
Rajat <light.rajat+gh@gmail.com>
2023-07-02 19:56:16 +0530

    Feature for custom spritesheet orientation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from os import listdir
 
+hor = False
 folder = "sprites"
 sprites = [i for i in listdir(folder)]
 
@@ -9,10 +10,10 @@ imgWidth = testImg.width
 imgHeight = testImg.height
 del testImg
 
-img = Image.new('RGBA', (imgWidth * len(sprites), imgHeight))
-x = 0
+img = Image.new('RGBA', (imgWidth * (len(sprites)**hor), imgHeight * ((len(sprites)**int(not(hor))))))
+co = 0
 for i in sprites:
     sprite = Image.open("{}/{}".format(folder, i))
-    img.paste(sprite, (x, 0))
-    x += imgWidth
+    img.paste(sprite, (co * hor, co * int(not(hor))))
+    co += imgWidth
 img.save("ss.png")
