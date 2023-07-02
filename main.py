384607536604868e6390f5caecb57556cb0830fd
Rajat <light.rajat+gh@gmail.com>
2023-07-02 16:44:31 +0530

    Tested workability for multiple files
    Tested with a loop over any number of files

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from PIL import Image
+from os import listdir
 
-img1 = Image.open("1.png")
-img2 = Image.open("2.png")
+folder = "sprites"
+sprites = [i for i in listdir(folder)]
 
-img = Image.new('RGBA', (128, 64))
-img.paste(img1, (0, 0))
-img.paste(img2, (img1.width, 0))
+img = Image.new('RGBA', (len(sprites) * 64, 64))
+x = 0
+for i in sprites:
+    sprite = Image.open("{}/{}".format(folder, i))
+    img.paste(sprite, (x, 0))
+    x += 64
 img.save("ss.png")
